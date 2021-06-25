499a16ca61921917d63c2307749c3e2fdb3216a0
Ilia_Stepanov <istepano@ru.estee.com>
2021-06-25 20:29:29 +0300

    Initial commit

---
+++ src/main/java/com/example/blabobot/BlabobotApplication.java
@@ -0,0 +1,13 @@
+package com.example.blabobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlabobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlabobotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blabobot/BlabobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blabobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlabobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
